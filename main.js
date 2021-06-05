60507960dccd73f37e8c8bd035a00bb7688371e0
poly28 <horimasa0328@gmail.com>
2021-06-05 16:25:08 +0900

    メッセージ改良

--- main.js
+++ main.js
@@ -5,34 +5,41 @@ new Vue({
 		boyShow: false,
 		girlShow: false,
 		boyMessage: false,
+		flashMessage0: false,
 		flashMessage1: false,
 		flashMessage2: false,
+		opacityClass: '',
 	},
 	methods: {
 		boyShowChange() {
 			this.mamaShow = false;
 			this.boyShow = true;
-
-			// setTimeout(() => {
-			// 	this.flashMessage1 = true;
-			// }, 3000);
 			setTimeout(this.showFlash1, 3000);
 		},
 		showFlash1() {
 			this.flashMessage1 = true;
-			setTimeout(this.boyHidden, 3000);
-			setTimeout(this.showFlash2, 3000);
+			setTimeout(this.opacityClass9, 500);
+			setTimeout(this.opacityClass7, 500);
+			setTimeout(this.opacityClass5, 500);
+			setTimeout(this.boySwitch, 3000); // 男の子非表示
+			setTimeout(this.girlSwitch, 3000); // 女の子表示
 		},
-		boyHidden() {
+		boySwitch() {
 			this.boyShow = false;
 		},
-		showFlash2() {
-			this.flashMessage1 = false;
+		girlSwitch() {
 			this.girlShow = true;
+			this.flashMessage1 = false;
 			this.flashMessage2 = true;
 		},
-		// girlVisible() {
-		// 	this.girlShow = true;
-		// },
+		opacityClass9() {
+			this.opacityClass = 'op9';
+		},
+		opacityClass7() {
+			this.opacityClass = 'op7';
+		},
+		opacityClass5() {
+			this.opacityClass = 'op5';
+		},
 	},
 });
